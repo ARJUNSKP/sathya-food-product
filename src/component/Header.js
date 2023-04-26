7f65f9a89304b646b8dc7f5480c1dc8f428e000c
Arjun <arjunsivadas2000@gmail.com>
2023-04-26 12:31:17 +0530

    add style

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Button,Container,Form,Nav,NavLink,Navbar} from 'react-bootstrap';
 import './Header.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from '../page/Home';
 
 function Header() {
   return (
-    <Navbar>
-        <Container>
+    <Navbar id="navbar">
+        <Container fluid>
             <Navbar.Brand href='#'>Sathya Foods</Navbar.Brand>
-            <Nav className='justify-content-end'id="nav" gap="6rem">
-                <NavLink href="#home">Home</NavLink>
+            <Routes>
+              <Nav className='justify-content-end navbar'id="nav" gap="6rem">
+                <NavLink href="#product">Home</NavLink>
                 <NavLink href="#product">Product</NavLink>
                 <NavLink href="#condact">Contact</NavLink>
-            </Nav>
-            <Form className="d-flex">
+              </Nav>
+            </Routes>
+            <Form className="d-flex search-bar">
                   <Form.Control
                     type="search"
                     placeholder="Search"
-                    className="me-2"
+                    className="me-1 size"
                     aria-label="Search"
                   />
                   <Button variant="outline-success">Search</Button>
+
             </Form>
-            <Nav className='justify-content-end'id="nav">
-                <Button variant="secondary" size="lg" active>Login </Button>
-                <Button variant="secondary" size="lg" active> Logout </Button>
+            <Nav className='justify-content-end'>
+                <Button variant="outline-success" size="sm" id="div1">Login</Button>
+                <Button variant="outline-secondary" size="sm" id="div2">Logout</Button>
             </Nav>
         </Container>
     </Navbar>
